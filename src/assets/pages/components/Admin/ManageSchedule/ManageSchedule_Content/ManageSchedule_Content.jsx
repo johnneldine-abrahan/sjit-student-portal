005caecf9877a10a692c2ae3811a0ffcc1a78487
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2024-09-03 22:29:10 +0800

    Update ManageSchedule_Content.jsx

---
+++ src/assets/pages/components/Admin/ManageSchedule/ManageSchedule_Content/ManageSchedule_Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ManageSchedule_Content.css'
+
+const ManageSchedule_Content = () => {
+  return (
+    <div className='ManageSchedule_content'>
+      
+    </div>
+  )
+}
+
+export default ManageSchedule_Content

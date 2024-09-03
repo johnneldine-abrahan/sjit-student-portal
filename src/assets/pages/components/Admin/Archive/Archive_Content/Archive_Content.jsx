6155d067ae36897b2386be4164e146e0217ae9d7
21-09622 <113603567+DanielIsaaiahCueto@users.noreply.github.com>
2024-09-03 22:04:07 +0800

    archive_content code insertion

---
+++ src/assets/pages/components/Admin/Archive/Archive_Content/Archive_Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Archive_Content.css'
+
+const Archive_Content = () => {
+  return (
+    <div className='archive_content'>
+      
+    </div>
+  )
+}
+
+export default Archive_Content

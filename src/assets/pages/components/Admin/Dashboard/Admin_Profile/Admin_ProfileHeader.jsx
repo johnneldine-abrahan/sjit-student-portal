cdea80786f519e777aceedd35fe7c05c2a3d515d
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-04 17:08:58 +0800

    logout placeholder function

--- src/assets/pages/components/Admin/Dashboard/Admin_Profile/Admin_ProfileHeader.jsx
+++ src/assets/pages/components/Admin/Dashboard/Admin_Profile/Admin_ProfileHeader.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import './Admin_Profile.css'
 import { BiEditAlt } from "react-icons/bi";
 import { LuLogOut } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 const Admin_ProfileHeader = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login')
+  }
+
   return (
     <div className="admin-p_header">
         <h2 className='profile-title'>Profile</h2>
@@ -12,7 +19,7 @@ const Admin_ProfileHeader = () => {
             <BiEditAlt class='profile-icon' />
           </div>
           <div className='profile-act'>
-            <LuLogOut className='profile-icon'/>
+            <LuLogOut className='profile-icon' onClick={handleLogout}/>
           </div>
         </div>
     </div>

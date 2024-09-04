500923d45c6fcd9c3980bee47fa984a935c93396
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-04 16:56:23 +0800

    dashboard 99% done na talaga

--- src/assets/pages/components/Admin/Dashboard/Admin_Profile/Admin_ProfileHeader.jsx
+++ src/assets/pages/components/Admin/Dashboard/Admin_Profile/Admin_ProfileHeader.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Admin_Profile.css'
 import { BiEditAlt } from "react-icons/bi";
+import { LuLogOut } from "react-icons/lu";
 
 const Admin_ProfileHeader = () => {
   return (
     <div className="admin-p_header">
         <h2 className='profile-title'>Profile</h2>
-        <div className='edit'>
-            <BiEditAlt class='edit-icon' />
+        <div className='buttons-header'>
+          <div className='profile-act'>
+            <BiEditAlt class='profile-icon' />
+          </div>
+          <div className='profile-act'>
+            <LuLogOut className='profile-icon'/>
+          </div>
         </div>
     </div>
   )

2781e348eaff5e190bae24d47afb501d48757440
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-04 18:14:26 +0800

    archive 5% front-end

--- src/assets/pages/components/Admin/Archive/Archive_Content/Archive_Content.jsx
+++ src/assets/pages/components/Admin/Archive/Archive_Content/Archive_Content.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Archive_Content.css'
+import Archive_ContentHeader from './Archive_ContentHeader'
 
 const Archive_Content = () => {
   return (
     <div className='archive_content'>
-      
+      <Archive_ContentHeader />
     </div>
   )
 }

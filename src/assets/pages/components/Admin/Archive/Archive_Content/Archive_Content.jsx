55290ff8e57eb0973dfa50a45e92910e61ef7bd3
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2024-09-04 13:36:44 -0400

    Update Archive_Content.jsx

--- src/assets/pages/components/Admin/Archive/Archive_Content/Archive_Content.jsx
+++ src/assets/pages/components/Admin/Archive/Archive_Content/Archive_Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Archive_Content.css'
 import Archive_ContentHeader from './Archive_ContentHeader'
+import Archive_Records from './Archive_Records'
 
 const Archive_Content = () => {
   return (
     <div className='archive_content'>
       <Archive_ContentHeader />
+      <Archive_Records/>
     </div>
   )
 }

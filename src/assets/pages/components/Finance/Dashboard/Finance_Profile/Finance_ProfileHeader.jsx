605376b7bc619c672fd0f727143ca5d18559da35
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-10 05:09:43 +0800

    finance logout placeholder function

--- src/assets/pages/components/Finance/Dashboard/Finance_Profile/Finance_ProfileHeader.jsx
+++ src/assets/pages/components/Finance/Dashboard/Finance_Profile/Finance_ProfileHeader.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import './Finance_Profile.css'
 import { BiEditAlt } from "react-icons/bi";
 import { LuLogOut } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 const Finance_ProfileHeader = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login')
+  }
+
   return (
     <div className='finance-profile-header'>
         <h2 className='profile-title'>Profile</h2>
@@ -12,7 +20,7 @@ const Finance_ProfileHeader = () => {
                 <BiEditAlt className='profile-icon' />
             </div>
             <div className='profile-act'>
-                <LuLogOut className='profile-icon' />
+                <LuLogOut className='profile-icon' onClick={handleLogout}/>
             </div>
         </div>
       

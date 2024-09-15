19fede1114df3035029c8f0073638890294518fe
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-15 09:00:43 +0800

    faculty pop-up content

--- src/assets/pages/components/Faculty/ActionCard_Faculty.jsx
+++ src/assets/pages/components/Faculty/ActionCard_Faculty.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import '../../Faculty/Faculty_module.css'
+import React, { useState } from 'react';
+import '../../Faculty/Faculty_module.css';
 
-const ActionCard_Faculty = ({icon, text}) => {
+const ActionCard_Faculty = ({ icon, text, content }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className='actionCard'>
-      <div className='actionIcon'>{icon}</div>
-      <div className='actionText'>{text}</div>
-    </div>
-  )
-}
+    <>
+      <div className='actionCard' onClick={handleCardClick}>
+        <div className='actionIcon'>{icon}</div>
+        <div className='actionText'>{text}</div>
+      </div>
 
-export default ActionCard_Faculty
+      {isModalOpen && (
+        <div className='modalOverlay'>
+          <div className='modal'>
+            <div className='modalHeader'>
+              <span className='modalTitle'>{text}</span>
+              <button className='modalCloseButton' onClick={handleCloseModal}>Close</button>
+            </div>
+            <div className='modalBody'>
+              {/* Use the content prop to display dynamic content */}
+              <p>{content}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ActionCard_Faculty;
+
--- src/assets/pages/components/Faculty/MainContent_Faculty.jsx
+++ src/assets/pages/components/Faculty/MainContent_Faculty.jsx
@@ -1,46 +1,50 @@
-import React from 'react'
-import '../../Faculty/Faculty_module.css'
-import ActionCard_Faculty from './ActionCard_Faculty'
+import React from 'react';
+import '../../Faculty/Faculty_module.css';
+import ActionCard_Faculty from './ActionCard_Faculty';
 import { AiFillSchedule } from "react-icons/ai";
 import { FaList } from "react-icons/fa";
 import { MdOutlineUploadFile } from "react-icons/md";
 import { MdAnalytics } from "react-icons/md";
 
 const actionItems = [
-    {
-        icon: <AiFillSchedule size={40} />,
-        text: "View Schedules"
-    },
-
-    {
-        icon: <FaList size={40} />,
-        text: "Student List"
-    },
-
-    {
-        icon: <MdOutlineUploadFile size={40} />,
-        text: "Upload Grades"
-    },
-
-    {
-        icon: <MdAnalytics size={40} />,
-        text: "Reports"
-    },
-]
+  {
+    icon: <AiFillSchedule size={40} />,
+    text: "View Schedules",
+    content: "Check your daily and weekly teaching schedules."
+  },
+  {
+    icon: <FaList size={40} />,
+    text: "Student List",
+    content: "View the list of students enrolled in your classes."
+  },
+  {
+    icon: <MdOutlineUploadFile size={40} />,
+    text: "Upload Grades",
+    content: "Upload students' grades for the current semester."
+  },
+  {
+    icon: <MdAnalytics size={40} />,
+    text: "Reports",
+    content: "Generate reports and analyze student performance data."
+  },
+];
 
 const MainContent_Faculty = () => {
   return (
     <section className='mainSection'>
-        <div className='mainBanner'>
-
-        </div>
-        <div className='actionGrid'>
-            {actionItems.map((item, index) => (
-                <ActionCard_Faculty key={index} icon={item.icon} text={item.text} />
-            ))}
-        </div>
+      <div className='mainBanner'></div>
+      <div className='actionGrid'>
+        {actionItems.map((item, index) => (
+          <ActionCard_Faculty 
+            key={index} 
+            icon={item.icon} 
+            text={item.text} 
+            content={item.content} // Pass specific content for each card
+          />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default MainContent_Faculty
+export default MainContent_Faculty;

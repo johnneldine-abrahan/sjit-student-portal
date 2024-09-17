848ea423c35200839b7911cdfe3e0f72f7245b11
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-17 19:34:20 +0800

    filter section student

--- src/assets/pages/components/Students/MainContent_Student.jsx
+++ src/assets/pages/components/Students/MainContent_Student.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import '../../Students/Students_module.css';
-import ActionCard_Student from './ActionCard_Student';
+import React from "react";
+import "../../Students/Students_module.css";
+import ActionCard_Student from "./ActionCard_Student";
 import { GrCertificate } from "react-icons/gr";
 import { AiFillSchedule } from "react-icons/ai";
 import { PiExamFill } from "react-icons/pi";
 import { FaReceipt } from "react-icons/fa";
 import { GiInjustice } from "react-icons/gi";
 import { IoMdPrint } from "react-icons/io";
-import Certificate_of_Registration from './Certificate_of_Registration/Certificate_of_Registration';
-import Subjects_and_Schedule from './Subjects_and_Schedule/Subjects_and_Schedule';
-import Grades_Students from './Grades_Students/Grades_Students';
-import Payments from './Payments/Payments';
-import Liabilities_Students from './Liabilities_Students/Liabilities_Students';
-import PrintCopyofGrades from './PrintCopyofGrades/PrintCopyofGrades';
+import { FiFilter } from "react-icons/fi";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import Certificate_of_Registration from "./Certificate_of_Registration/Certificate_of_Registration";
+import Subjects_and_Schedule from "./Subjects_and_Schedule/Subjects_and_Schedule";
+import Grades_Students from "./Grades_Students/Grades_Students";
+import Payments from "./Payments/Payments";
+import Liabilities_Students from "./Liabilities_Students/Liabilities_Students";
+import PrintCopyofGrades from "./PrintCopyofGrades/PrintCopyofGrades";
 import { Carousel } from "react-responsive-carousel";
 import image1 from "../../../img/Faculty/Pinas.jpg";
 import image2 from "../../../img/Faculty/RHU.jpg";
@@ -22,39 +24,39 @@ const actionItems = [
   {
     icon: <GrCertificate size={40} />,
     text: "Certificate of Registration",
-    content: <Certificate_of_Registration />
+    content: <Certificate_of_Registration />,
   },
   {
     icon: <AiFillSchedule size={40} />,
     text: "Subjects and Schedule",
-    content: <Subjects_and_Schedule />
+    content: <Subjects_and_Schedule />,
   },
   {
     icon: <PiExamFill size={40} />,
     text: "Grades",
-    content: <Grades_Students />
+    content: <Grades_Students />,
   },
   {
     icon: <FaReceipt size={40} />,
     text: "Payments",
-    content: <Payments />
+    content: <Payments />,
   },
   {
     icon: <GiInjustice size={40} />,
     text: "Liabilities",
-    content: <Liabilities_Students />
+    content: <Liabilities_Students />,
   },
   {
     icon: <IoMdPrint size={40} />,
     text: "Print Copy of Grades",
-    content: <PrintCopyofGrades />
+    content: <PrintCopyofGrades />,
   },
 ];
 
 const MainContent_Student = () => {
   const bannerImages = [image1, image2, image3];
   return (
-    <section className='mainSection'>
+    <section className="mainSection">
       <Carousel
         autoPlay={true}
         interval={3000}
@@ -62,20 +64,30 @@ const MainContent_Student = () => {
         showArrows={true}
         stopOnHover={false}
         transitionTime={500}
-        >
-          
+      >
         {bannerImages.map((image, index) => (
           <div key={index}>
             <img src={image} alt={`Pinas.jpg ${index + 1}`} />
           </div>
         ))}
       </Carousel>
-      <div className='actionGrid'>
+
+      <div className="filter-section">
+          <p><IoIosInformationCircleOutline size={30} className="info-ico"/>Please click "Filter" button to change the current school year / semester</p>
+          <div className="filterheader-section">
+            <div className="filterheader">
+              <FiFilter className="filter-ico" />
+              <h3 className="filtertitle">Filter</h3>
+            </div>
+          </div>
+      </div>
+
+      <div className="actionGrid">
         {actionItems.map((item, index) => (
-          <ActionCard_Student 
-            key={index} 
-            icon={item.icon} 
-            text={item.text} 
+          <ActionCard_Student
+            key={index}
+            icon={item.icon}
+            text={item.text}
             content={item.content} // Pass specific content for each card
           />
         ))}

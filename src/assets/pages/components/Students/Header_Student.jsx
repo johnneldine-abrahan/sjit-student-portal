a339d78a93f6554e23e820548b1d9742c6ff2d55
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-19 03:12:29 +0800

    log-out pop-up student faculty

--- src/assets/pages/components/Students/Header_Student.jsx
+++ src/assets/pages/components/Students/Header_Student.jsx
@@ -1,15 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../../Students/Students_module.css'
 import logo from '../../../img/LandingPage/NavBar/logo.png'
 import { LuLogOut } from "react-icons/lu";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState({
+    show: false,
+    message: '',
+  });
+
+  const handleLogout = () => {
+    setIsModalOpen({
+      show: true,
+      message: 'Are you sure you want to log out?',
+    });
+  };
+
+  const handleClose = () => {
+    setIsModalOpen({
+      show: false,
+      message: '',
+    });
+  };
+
+  const handleConfirmLogout = () => {
+    console.log('Logging out...');
+    setIsModalOpen({
+      show: false,
+      message: '',
+    });
+  };
+
+  useEffect(() => {
+    if (isModalOpen.show) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset'; // Reset overflow when modal is closed
+    }
+
+    // Clean up the effect when the component unmounts or modal closes
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isModalOpen.show]);
+
+
   return (
     <header className='headerStudent'>
       <img src={logo} className='logo-student' />
-      <button className='btn log-out'>Log out<LuLogOut className='btn-img' /></button>
+      <button
+        className='btn log-out'
+        onClick={handleLogout}
+      >
+        Log out<LuLogOut className='btn-img' />
+      </button>
+
+      {isModalOpen.show && (
+        <div className='modalOverlay' onClick={handleClose} />
+      )}
+      {isModalOpen.show && (
+        <div className='modal'>
+          <div className='modalHeader'>
+            <h3 className='modalTitle'>Log out</h3>
+            <button className='modalCloseButton' onClick={handleClose}>Close</button>
+          </div>
+          <div className='modalBody'>
+            <p>{isModalOpen.message}</p>
+            <button onClick={handleConfirmLogout}>Confirm</button>
+          </div>
+        </div>
+      )}
     </header>
-  )
-}
+  );
+};
 
 export default Header

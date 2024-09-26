4f0c8289ac11fb6fb8340c6f7763a2a8ab567310
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-09-26 08:19:00 +0800

    tag-liab front-end

--- src/assets/pages/components/Finance/Finance_TagLiabilities/TagLiabilities_Content/TagLiabilities_Content.jsx
+++ src/assets/pages/components/Finance/Finance_TagLiabilities/TagLiabilities_Content/TagLiabilities_Content.jsx
@@ -1,14 +1,105 @@
-import React from 'react'
-import './TagLiabilities_Content.css'
-import TagLiabilities_ContentHeader from './TagLiabilities_ContentHeader'
+import React, { useState } from "react";
+import "./TagLiabilities_Content.css";
+import TagLiabilities_ContentHeader from "./TagLiabilities_ContentHeader";
+
+
+const StudentLiabRecords = [
+  {
+      studentID: '21-05298',
+      LastName: 'Sanchez',
+      FirstName: 'Kim William',
+      MiddleName: 'Bacsa',
+      yearGraduated: '2025',
+      status:'Active',
+  },
+  {
+      studentID: '21-05298',
+      LastName: 'Sanchez',
+      FirstName: 'Kim William',
+      MiddleName: 'Bacsa',
+      yearGraduated: '2025',
+      status:'Active',
+  },
+  {
+      studentID: '21-05298',
+      LastName: 'Sanchez',
+      FirstName: 'Kim William',
+      MiddleName: 'Bacsa',
+      yearGraduated: '2025',
+      status:'Active',
+  },
+  {
+      studentID: '21-05298',
+      LastName: 'Sanchez',
+      FirstName: 'Kim William',
+      MiddleName: 'Bacsa',
+      yearGraduated: '2025',
+      status:'Active',
+  },
+]
 
 const TagLiabilities_Content = () => {
-  return (
-    <div className='tagliabilities-content'>
-        <TagLiabilities_ContentHeader />
-      
-    </div>
-  )
-}
+  const [popup, setPopup] = useState({
+    show: false,
+    record: null,
+  });
 
-export default TagLiabilities_Content
+  const handlePopup = (record) => {
+    setPopup({
+      show: true,
+      record: record,
+    });
+  };
+
+  const handleClose = () => {
+    setPopup({
+      show: false,
+      record: null,
+    });
+  };
+  return (
+    <div className="tagliabilities-content">
+      <TagLiabilities_ContentHeader />
+      <div className="student-records">
+        <div className="recordslist-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Select</th> {/* New column for checkbox */}
+                <th>Student ID</th>
+                <th>Last Name</th>
+                <th>First Name</th>
+                <th>Middle Name</th>
+                <th>Year Graduated</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {StudentLiabRecords.map((records, index) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      name={`select-${records.studentID}`}
+                    />{" "}
+                    {/* Checkbox */}
+                  </td>
+                  <td>{records.studentID}</td>
+                  <td>{records.LastName}</td>
+                  <td>{records.FirstName}</td>
+                  <td>{records.MiddleName}</td>
+                  <td>{records.yearGraduated}</td>
+                  <td>{records.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default TagLiabilities_Content;
--- src/assets/pages/components/Finance/Finance_TagLiabilities/TagLiabilities_Content/TagLiabilities_ContentHeader.jsx
+++ src/assets/pages/components/Finance/Finance_TagLiabilities/TagLiabilities_Content/TagLiabilities_ContentHeader.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import './TagLiabilities_Content.css'
+import React, { useState, useEffect } from "react";
+import "./TagLiabilities_Content.css";
 import { BiSearch } from "react-icons/bi";
+
+import { BiEditAlt } from "react-icons/bi";
+import { RiAddLargeFill, RiDeleteBin6Line } from "react-icons/ri";
+import { RiInboxUnarchiveLine } from "react-icons/ri";
 
 const TagLiabilities_ContentHeader = () => {
   return (
-    <div className='tagliabilities-header'>
-        <h1 className='header-title'>Tag Liabilities</h1>
-        <div className='tagliabilities-activity'>
-            <div className='search-box'>
-                <input type="text" placeholder='Search...' />
-                <BiSearch className='search-icon' />
-            </div>
+    <div className="tagliabilities-header">
+      <h1 className="header-title">Tag Liabilities</h1>
+      <div className="tagliabilities-activity">
+        <div className="search-box">
+          <input type="text" placeholder="Search..." />
+          <BiSearch className="search-icon" />
         </div>
-      
+        <div className="buttons-header">
+          <div className="buttons-act">
+            <RiAddLargeFill
+              className="buttons-icon"
+              onClick={() => handlePopup("add")}
+            />
+          </div>
+          <div className="buttons-act">
+            <BiEditAlt
+              className="buttons-icon"
+              onClick={() => handlePopup("edit")}
+            />
+          </div>
+          <div className="buttons-act">
+            <RiDeleteBin6Line
+              className="buttons-icon"
+              onClick={() => handlePopup("delete")}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TagLiabilities_ContentHeader
+export default TagLiabilities_ContentHeader;

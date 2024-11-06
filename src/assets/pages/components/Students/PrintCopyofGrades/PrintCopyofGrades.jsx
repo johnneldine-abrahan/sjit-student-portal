b86050398437b2cca9dae4fd1fdb6e0d1eb2d875
21-09622 <113603567+DanielIsaaiahCueto@users.noreply.github.com>
2024-11-06 14:53:33 +0800

    Update PrintCopyofGrades.jsx
    Fix Print

--- src/assets/pages/components/Students/PrintCopyofGrades/PrintCopyofGrades.jsx
+++ src/assets/pages/components/Students/PrintCopyofGrades/PrintCopyofGrades.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { IoMdPrint } from 'react-icons/io'; // Corrected import for the icon
+import './PrintCopyofGrades.css';
 
 const PrintCopyofGrades = () => {
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
-    <div>
-      <p>Back-end din ata to mga boss hahahahha</p>
+    // Print button with icon wrapped in a span
+    <div className="button-container">
+      <button className="btn-box" onClick={handlePrint}>
+        <span className="print-icon">
+          <IoMdPrint />
+        </span>
+        Print
+      </button>
     </div>
-  )
-}
+  );
+};
+
 
 export default PrintCopyofGrades

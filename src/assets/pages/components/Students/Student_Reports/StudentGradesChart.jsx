e81df67abf0ea9d8922e5d45274dbed3148d7161
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-11-19 22:44:13 +0800

    bar graph fix

--- src/assets/pages/components/Students/Student_Reports/StudentGradesChart.jsx
+++ src/assets/pages/components/Students/Student_Reports/StudentGradesChart.jsx
@@ -11,16 +11,17 @@ import {
 
 const StudentGradesChart = ({ gradesData }) => {
   // Format the grades data for the chart
-  const chartData = gradesData.map(grade => ({
+  const chartData = gradesData.map((grade) => ({
     subject: grade.subject_name,
-    grade: grade.grade
+    grade: grade.grade,
   }));
 
   return (
     <BarChart width={500} height={500} data={chartData}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="subject" />
-      <YAxis />
+      {/* Set the ground number to 75 and customize ticks */}
+      <YAxis ticks={[70, 75, 80, 85, 90, 95, 100]} domain={[70, 100]} />
       <Tooltip />
       <Legend />
       <Bar dataKey="grade" fill="#7b8bbc" />

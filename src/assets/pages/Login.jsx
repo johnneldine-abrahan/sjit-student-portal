ed97d0d9a23842086d530556fcb853753518c777
johnneldine-abrahan <21-02497@g.batstate-u.edu.ph>
2024-11-21 19:20:36 +0800

    start deploy

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("https://san-juan-institute-of-technology-backend.onrender.com/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
